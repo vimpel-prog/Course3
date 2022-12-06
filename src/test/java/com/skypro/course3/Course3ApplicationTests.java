7238325065c12fa9cda0caf135be3414471b7bda
Sergey <vimpel812@gmail.com>
2022-12-06 23:58:10 +0100

    Initial commit

---
+++ src/test/java/com/skypro/course3/Course3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.course3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
